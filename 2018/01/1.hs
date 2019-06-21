cbe42e007d53e08940f92d83bba1e7e7ef5e1057
Andres Schmois <andres@itpro.tv>
2019-06-21 16:56:08 -0400

    Replace lambda call with id

--- 2018/01/1.hs
+++ 2018/01/1.hs
@@ -3,7 +3,8 @@ main = do
     content <- getContents -- We need to pull contents out of IO. getConents returns IO String, we want String
     let contentLines = lines content :: [String] -- creates lines of contents
     let convertedFunctions = map convert contentLines :: [Int -> Int] -- creates an array of functions that get an int and return an int
-    let result = foldr (\ fn int -> fn int) 0 convertedFunctions -- reduce a function starting from 0 with an array of functions
+    -- let foo = scanl id 0 convertedFunctions
+    let result = foldr id 0 convertedFunctions -- reduce a function starting from 0 with an array of functions
     print result
 
 convert :: String -> (Int -> Int)

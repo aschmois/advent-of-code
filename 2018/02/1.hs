59a8bf5ca8a13e773d6e4e463ab9ceb5ff6540d7
Andres Schmois <andres.schmois@gmail.com>
2019-07-01 10:14:01 -0400

    Finish day 2 part 1

--- 2018/02/1.hs
+++ 2018/02/1.hs
@@ -4,29 +4,30 @@
 -- for ghcid : stack --resolver lts-13.20 exec --package ghcid -- ghcid 1.hs --test :main
 
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 main :: IO ()
 main = do
   content <- readFile "input.txt"
   print 
+    . f3
     . f2 
     . map f1 
     . lines 
     $ content
 
-f2 :: [(Int, Int)] -> Int
-f2 = foldr (\ (e1, e2) a -> a + e1 * e2 ) 0
+f3 :: (Int, Int) -> Int
+f3 (e1, e2) = e1 * e2
+
+f2 :: [(Int, Int)] -> (Int, Int)
+f2 = foldr (\ (e1, e2) (a1, a2) -> (a1 + e1, a2 + e2)) (0,0)
 
 f1 :: String -> (Int, Int)
 f1 cLine = do
     let csMap = f1a cLine
         cs = Map.elems csMap
-    (boolToInt $ elem 2 cs, boolToInt $ elem 3 cs)
-
-boolToInt :: Bool -> Int
-boolToInt a = case a of
-    True -> 1
-    False -> 0
+        xs = Set.fromList cs
+    (fromEnum $ Set.member 2 xs, fromEnum $ Set.member 3 xs)
 
 f1a :: [Char] -> Map.Map Char Int
-f1a cs = undefined
+f1a = foldr (\ char -> Map.insertWith (+) char 1) Map.empty 
